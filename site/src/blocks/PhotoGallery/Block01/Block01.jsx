6c61bcc5afe110e7e8f91d037e064347cff6b0ed
ElegantStack <elegantstack.studio@gmail.com>
2021-10-19 06:09:08 -0700

    add click to enlarge to PhotoGallery block

--- site/src/blocks/PhotoGallery/Block01/Block01.jsx
+++ site/src/blocks/PhotoGallery/Block01/Block01.jsx
@@ -1,30 +1,89 @@
-import React from 'react'
-import { Container, Grid, Box } from 'theme-ui'
+import React, { useContext } from 'react'
+import { Container, Grid, Flex } from 'theme-ui'
 import Reveal from '@solid-ui-components/Reveal'
 import Divider from '@solid-ui-components/Divider'
 import ContentImages from '@solid-ui-components/ContentImages'
 import ContentButtons from '@solid-ui-components/ContentButtons'
 import ContentText from '@solid-ui-components/ContentText'
+import Modal, { ModalContext } from '@solid-ui-components/Modal'
 import WithDefaultContent from '@solid-ui-blocks/WithDefaultContent'
+import { FaSearch } from 'react-icons/fa'
 
-const PhotoGalleryBlock01 = ({ content: { text, collection, buttons } }) => (
-  <Container sx={{ textAlign: `left` }}>
-    <ContentText content={text} />
-    <Divider />
-    <Grid columns={[1, 2, 3]}>
-      {collection?.map(({ images }, i) => (
-        <Reveal effect='fadeIn'>
-          <ContentImages key={`item=${i}`} content={{ images }} />
-        </Reveal>
-      ))}
-    </Grid>
-    {buttons && (
-      <>
-        <Divider space={3} />
-        <ContentButtons content={buttons} />
-      </>
-    )}
-  </Container>
-)
+const styles = {
+  item: {
+    cursor: `pointer`,
+    position: `relative`,
+    ':hover': {
+      '.gallery-overlay': {
+        opacity: 1
+      }
+    }
+  },
+  overlay: {
+    borderRadius: `default`,
+    alignItems: `center`,
+    justifyContent: `center`,
+    size: `full`,
+    bg: `rgba(50,50,50,0.4)`,
+    transition: `opacity 100ms ease-in`,
+    opacity: 0,
+    position: `absolute`,
+    zIndex: 2,
+    top: 0,
+    left: 0
+  },
+  modal: {
+    width: `fit-content`,
+    p: 3
+  }
+}
+
+const PhotoGalleryBlock01 = ({
+  content: { identifier, text, collection, buttons }
+}) => {
+  const { setActiveModal } = useContext(ModalContext)
+
+  return (
+    <Container sx={{ textAlign: `center` }}>
+      <ContentText content={text} />
+      <Divider />
+      <Grid columns={[1, 2, 3]}>
+        {collection?.map(({ images }, i) => {
+          const modalIdentifier = `${identifier}${i}-image`
+          return (
+            <React.Fragment key={`item-${i}`}>
+              <Reveal
+                effect='fadeIn'
+                onClick={() => setActiveModal(modalIdentifier)}
+                css={styles.item}
+              >
+                <Flex sx={styles.overlay} className='gallery-overlay'>
+                  <FaSearch size='2.4em' color='white' />
+                </Flex>
+                <ContentImages content={{ images: [images[0]] }} />
+              </Reveal>
+              <Modal
+                id={modalIdentifier}
+                contentLabel='Image'
+                contentStyles={styles.modal}
+              >
+                <ContentImages
+                  content={{ images: [images[1]] }}
+                  imageEffect='fadeIn'
+                />
+              </Modal>
+            </React.Fragment>
+          )
+        })}
+      </Grid>
+      {buttons && (
+        <>
+          <Divider space={3} />
+          <ContentButtons content={buttons} />
+        </>
+      )}
+    </Container>
+  )
+}
 
 export default WithDefaultContent(PhotoGalleryBlock01)
