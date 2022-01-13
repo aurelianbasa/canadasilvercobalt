cd188c268c6bb91039f27bbdf995e9c6f44b44b6
Aurelian Basa <aurelianbasa@live.ca>
2022-01-13 13:17:18 -0500

    Update _styles.js

--- site/src/pages/_styles.js
+++ site/src/pages/_styles.js
@@ -22,8 +22,8 @@ export default {
     '::after': {
       background: `linear-gradient(
           180deg,
-          rgba(255,255,255,0.4) 0%,
-          rgba(255,255,255,0.6) 100%
+          rgba(255,255,255,0.1) 0%,
+          rgba(255,255,255,0.3) 100%
         )`
     },
     '::before': {
