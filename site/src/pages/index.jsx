6ae3210d8e477d873c0ed3ce6bf7034cddcb2acf
Aurelian Basa <aurelianbasa@live.ca>
2022-01-25 15:44:45 -0500

    changes to be revised again

--- site/src/pages/index.jsx
+++ site/src/pages/index.jsx
@@ -36,7 +36,7 @@ const IndexPage = props => {
       <ModalSimple content={content['disclaimer']} />
       {/* Blocks */}
       <Header content={content['header']} />
-      <Divider space='6' />
+      <Divider space='5' />
       <Divider space='5' />
       <Container variant='wide' sx={styles.heroContainer}>
         <Hero content={content['hero']} reverse />
