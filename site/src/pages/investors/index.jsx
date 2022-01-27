c65b88985d25ccaf016ca9d77b9e9ebbd9f09b52
Aurelian Basa <aurelianbasa@live.ca>
2022-01-27 12:03:39 -0500

    maybe now it will work

--- site/src/pages/investors/index.jsx
+++ site/src/pages/investors/index.jsx
@@ -91,10 +91,10 @@ const MediaPage = props => {
             <tbody>
               <tr>
                 <td>2021</td>
-                <td><a href='/site/content/assets/site/investors/financials/2021/2021-Q2-FS-CCW.pdf'>Q1</a></td>
-                <td><a href='../../../downloads/financials/2021-Q2-FS-CCW.pdf'>Q2</a></td>
-                <td>Q3</td>
-                <td>Q4</td>
+                <td><a href='/files/Q2.pdf'>Q1</a></td>
+                <td><a href='files/Q2.pdf'>Q2</a></td>
+                <td><a href='files/Q2.pdf' download>Q2</a></td>
+                <td><a href='/files/Q2.pdf' download>Q1</a></td>
               </tr>
               <tr>
               <td>2020</td>
