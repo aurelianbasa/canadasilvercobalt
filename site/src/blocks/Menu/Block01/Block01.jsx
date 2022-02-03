9e01055d379859f85ffece7143c821af8e158bf1
Aurelian Basa <aurelianbasa@live.ca>
2022-02-03 17:11:36 -0500

    Update Block01.jsx

--- site/src/blocks/Menu/Block01/Block01.jsx
+++ site/src/blocks/Menu/Block01/Block01.jsx
@@ -8,57 +8,11 @@ import ContentImages from '@solid-ui-components/ContentImages'
 import ContentButtons from '@solid-ui-components/ContentButtons'
 import WithDefaultContent from '@solid-ui-blocks/WithDefaultContent'
 
-const styles = {
-  wrapper: {
-    position: `relative`,
-    zIndex: 10,
-    '.nav-container': {
-      bg: `headerBg`,
-      position: `fixed`,
-      transition: `all 250ms ease-in`,
-      // overflow: `hidden`,
-      py: 3
-    },
-    '.nav-sticky .nav-container': {
-      bg: `headerActiveBg`,
-      boxShadow: `0 0 25px rgba(140,152,164,.25)`,
-      py: [3, null, 2],
-      '.button-group-link.level-1, button-group-link.level-1:visited': {
-        color: `headerActiveColor`
-      }
-    },
-    //Make buttons in header smaller
-    '.button-group-button': {
-      minWidth: 120,
-      fontSize: 1,
-      px: 3,
-      py: 1
-    }
-  },
-  header: {
-    justifyContent: `space-between`,
-    alignItems: `center`
-    // height: [`6rem`, `7rem`], //prevent layout shift
-  },
-  logoContainer: {
-    flexShrink: 0,
-    mr: [null, null, 3, 5]
-  },
-  desktopMenu: {
-    display: [`none`, null, `block`],
-    minWidth: `auto`,
-    flexGrow: 1
-  },
-  mobileMenu: {
-    display: [`block`, null, `none`]
-  }
-}
-
 const HeaderBlock01 = ({ content: { images, collection }, menuJustify }) => {
   return (
     <>
       <Sticky
-        enabled='true'
+        enabled='false'
         stickyClassName='nav-sticky'
         css={css(styles.wrapper)}
       >
