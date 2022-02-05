1861d751ed4a0734f819ae339ff865c897ff090f
Aurelian Basa <aurelianbasa@live.ca>
2022-02-05 14:38:14 -0500

    Update index.jsx

--- site/src/pages/investors/index.jsx
+++ site/src/pages/investors/index.jsx
@@ -5,6 +5,7 @@ import Layout from '@solid-ui-layout/Layout'
 import Seo from '@solid-ui-components/Seo'
 import Divider from '@solid-ui-components/Divider'
 import ModalSimple from '@solid-ui-blocks/Modal/Block02'
+import Materials from '@solid-ui-blocks/Hero/Block03'
 import Content from '@solid-ui-blocks/Content/Block02'
 import Header from '@solid-ui-blocks/Header/Block01'
 import DropDown from '@solid-ui-blocks/Faq/Block01'
@@ -38,9 +39,9 @@ const MediaPage = props => {
       <Table content={content['capital-structure']} />
       <Divider space='5' />
       <Container variant='narrow' sx={styles.captionContainer}>
-      <Content content={content['materials-intro']} />
+      <Materials content={content['materials-intro']} />
       <Divider space='2' />
-      <Content content={content['presentations']} />
+      <Materials content={content['presentations']} />
       </Container>
       
       <Divider space='5' />
