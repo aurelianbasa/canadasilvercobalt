1097ea062f94945b923f0daa6e7752df3c6b1afe
aurelianbasa <56235624+aurelianbasa@users.noreply.github.com>
2022-03-09 21:10:57 -0500

    will it work?

--- site/src/pages/index.jsx
+++ site/src/pages/index.jsx
@@ -40,7 +40,7 @@ const IndexPage = props => {
       <Divider space='5' />
       <Container variant='wide' sx={styles.heroContainer}>
         <Hero content={content['hero']} reverse />
-        <Divider space={-3} />
+        <Divider space={-4} />
       </Container>
       <Divider space='2' />
       <Divider space='3' />
