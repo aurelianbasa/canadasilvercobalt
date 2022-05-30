777423f014eeed689c9283a6e1523f0f9ea9b087
ElegantStack <elegantstack.studio@gmail.com>
2022-05-30 07:12:15 -0700

    change twitter summary card to large

--- site/src/@elegantstack/flow-ui-widgets/Seo/Seo.jsx
+++ site/src/@elegantstack/flow-ui-widgets/Seo/Seo.jsx
@@ -32,6 +32,8 @@ const Seo = ({
     imageSrc &&
     (imageSrc.startsWith('//') ? imageSrc : siteUrl && `${siteUrl}${imageSrc}`)
 
+  console.log(getImageVariant(thumbnail, 'hero'), imageUrl)
+
   /**
    * Meta Tags
    */
@@ -47,7 +49,7 @@ const Seo = ({
     { property: 'og:site_name', content: site.name },
     { property: 'og:image', content: imageUrl },
 
-    { name: 'twitter:card', content: 'summary' },
+    { name: 'twitter:card', content: 'summary_large_image' },
     { name: 'twitter:site', content: site.name },
     { name: 'twitter:title', content: title },
     { name: 'twitter:description', content: description },
