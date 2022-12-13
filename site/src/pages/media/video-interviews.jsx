212fef290fa325b01a7eb51767539acda39e3259
Aurelian Basa <aurelianbasa@live.ca>
2022-12-13 13:16:20 -0500

    latest videos

--- site/src/pages/media/video-interviews.jsx
+++ site/src/pages/media/video-interviews.jsx
@@ -23,6 +23,8 @@ const MediaPage = props => {
       <Header content={content['header']} />
       <Divider space='5' />
       <Divider space='5' />
+      <Cards content={content['video-interviews-15']} />
+      <Divider space='1' />
       <Cards content={content['video-interviews-14']} />
       <Divider space='1' />
       <Cards content={content['video-interviews-13']} />
