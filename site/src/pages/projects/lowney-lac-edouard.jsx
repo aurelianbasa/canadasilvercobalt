885f5b67e3d6cfb324e8d161b221da2996909de1
Aurelian Basa <aurelianbasa@live.ca>
2022-12-13 11:21:50 -0500

    Lowney lac edouard page added

---
+++ site/src/pages/projects/lowney-lac-edouard.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import { Container } from 'theme-ui'
+import Layout from '@solid-ui-layout/Layout'
+import Seo from '@solid-ui-components/Seo'
+import Divider from '@solid-ui-components/Divider'
+import ModalSimple from '@solid-ui-blocks/Modal/Block02'
+import Header from '@solid-ui-blocks/Header/Block01'
+import Info from '@solid-ui-blocks/FeaturesWithPhoto/Block01'
+import Location from '@solid-ui-blocks/FeaturesWithPhoto/Block03'
+import Footer from '@solid-ui-blocks/Footer/Block01'
+import { normalizeBlockContentNodes } from '@blocks-helpers'
+
+const ProjectItemPage = props => {
+  const { allBlockContent } = props.data
+  const content = normalizeBlockContentNodes(allBlockContent?.nodes)
+
+  return (
+    <Layout {...props}>
+      <Seo title='Lowney Lac Edouard' />
+      {/* Modals */}
+      <ModalSimple content={content['privacy-policy']} />
+      <ModalSimple content={content['disclaimer']} />
+      {/* Blocks */}
+      <Header content={content['header']} />
+      <Divider space='5' />
+      <Divider space='5' />
+      <Info content={content['overview']} />
+      <Divider space='5' />
+      <Container variant='narrow'>
+        <Location content={content['location']} />
+        <Divider space='5' />
+      </Container>
+      <Divider space='5' />
+      <Footer content={content['footer']} />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query siteProjectsLowneyLacEdouardBlockContent {
+    allBlockContent(
+      filter: { page: { in: ["site/projects/lowney-lac-edouard", "site/shared"] } }
+    ) {
+      nodes {
+        ...BlockContent
+      }
+    }
+  }
+`
+export default ProjectItemPage
