83dbceb36661bcfe0f42a2a1e328c67bf55599ad
aurelianbasa <56235624+aurelianbasa@users.noreply.github.com>
2024-01-22 10:07:16 -0500

    Update index.jsx

--- site/src/pages/index.jsx
+++ site/src/pages/index.jsx
@@ -40,7 +40,7 @@ const IndexPage = props => {
       {/* Blocks */}
       <Header content={content['header']} />
       <Divider space='5' />
-      <Divider space='4' />
+      <Divider space='5' />
       <Container variant='wide' sx={styles.heroContainer}>
         <Hero content={content['hero']} reverse />
         <Divider space={-5} />
